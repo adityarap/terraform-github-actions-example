c2b0597a3a1b35a594daf57195ae9b293850d3d3
adityarap <91215774+adityarap@users.noreply.github.com>
2023-01-24 16:26:29 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,90 +1,90 @@
-import boto3
-from botocore.exceptions import ClientError
+# import boto3
+# from botocore.exceptions import ClientError
 
-SENDER = "testfor276@gmail.com" # must be verified in AWS SES Email
-RECIPIENT = "testfor276@gmail.com" # must be verified in AWS SES Email
+# SENDER = "testfor276@gmail.com" # must be verified in AWS SES Email
+# RECIPIENT = "testfor276@gmail.com" # must be verified in AWS SES Email
 
-# If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
-AWS_REGION = "us-east-1"
+# # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
+# AWS_REGION = "us-east-1"
 
-# The subject line for the email.
-SUBJECT = "This is test email for testing purpose..!!"
+# # The subject line for the email.
+# SUBJECT = "This is test email for testing purpose..!!"
 
-# The email body for recipients with non-HTML email clients.
-BODY_TEXT = ("Hey Hi...\r\n"
-             "This email was sent with Amazon SES using the "
-             "AWS SDK for Python (Boto)."
-            )
+# # The email body for recipients with non-HTML email clients.
+# BODY_TEXT = ("Hey Hi...\r\n"
+#              "This email was sent with Amazon SES using the "
+#              "AWS SDK for Python (Boto)."
+#             )
             
-# The HTML body of the email.
-BODY_HTML = """<html>
-<head></head>
-<body>
-  <h1>Hey Hi...</h1>
-  <p>This email was sent with
-    <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
-    <a href='https://aws.amazon.com/sdk-for-python/'>
-      AWS SDK for Python (Boto)</a>.</p>
-</body>
-</html>
-            """            
+# # The HTML body of the email.
+# BODY_HTML = """<html>
+# <head></head>
+# <body>
+#   <h1>Hey Hi...</h1>
+#   <p>This email was sent with
+#     <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+#     <a href='https://aws.amazon.com/sdk-for-python/'>
+#       AWS SDK for Python (Boto)</a>.</p>
+# </body>
+# </html>
+#             """            
 
-# The character encoding for the email.
-CHARSET = "UTF-8"
+# # The character encoding for the email.
+# CHARSET = "UTF-8"
 
-# Create a new SES resource and specify a region.
-client = boto3.client('ses',region_name=AWS_REGION)
+# # Create a new SES resource and specify a region.
+# client = boto3.client('ses',region_name=AWS_REGION)
 
-# Try to send the email.
-try:
-    #Provide the contents of the email.
-    response = client.send_email(
-        Destination={
-            'ToAddresses': [
-                RECIPIENT,
-            ],
-        },
-        Message={
-            'Body': {
-                'Html': {
+# # Try to send the email.
+# try:
+#     #Provide the contents of the email.
+#     response = client.send_email(
+#         Destination={
+#             'ToAddresses': [
+#                 RECIPIENT,
+#             ],
+#         },
+#         Message={
+#             'Body': {
+#                 'Html': {
     
-                    'Data': BODY_HTML
-                },
-                'Text': {
+#                     'Data': BODY_HTML
+#                 },
+#                 'Text': {
     
-                    'Data': BODY_TEXT
-                },
-            },
-            'Subject': {
+#                     'Data': BODY_TEXT
+#                 },
+#             },
+#             'Subject': {
 
-                'Data': SUBJECT
-            },
-        },
-        Source=SENDER
-    )
-# Display an error if something goes wrong.	
-except ClientError as e:
-    print(e.response['Error']['Message'])
-else:
-    print("Email sent! Message ID:"),
-    print(response['MessageId'])
-# import smtplib, ssl
-# import os
+#                 'Data': SUBJECT
+#             },
+#         },
+#         Source=SENDER
+#     )
+# # Display an error if something goes wrong.	
+# except ClientError as e:
+#     print(e.response['Error']['Message'])
+# else:
+#     print("Email sent! Message ID:"),
+#     print(response['MessageId'])
+import smtplib, ssl
+import os
 
 
-# port = 465
-# smtp_server = "smtp.gmail.com"
-# USERNAME = os.environ.get('USER_EMAIL')
-# PASSWORD = os.environ.get('USER_PASSWORD')
-# connection_url: ${{secrets.MAIL_CONNECTION}}
-# message = """\
-# Subject: ${{ github.job }} job of ${{ github.repository }} has ${{ job.status }}
-# to: aditya.raparthi13@gmail.com
+port = 465
+smtp_server = "smtp.gmail.com"
+USERNAME = os.environ.get('USER_EMAIL')
+PASSWORD = os.environ.get('USER_PASSWORD')
+connection_url: ${{secrets.MAIL_CONNECTION}}
+message = """\
+Subject: ${{ github.job }} job of ${{ github.repository }} has ${{ job.status }}
+to: testfor276@gmail.com
 
-# This is your daily email report.
-# """
+This is your daily email report.
+"""
 
-# context = ssl.create_default_context()
-# with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-#     server.login(USERNAME,PASSWORD)
-#     server.sendmail(USERNAME,USERNAME,message)
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(USERNAME,PASSWORD)
+    server.sendmail(USERNAME,USERNAME,message)
